83b4779a3e872b55d0ecd4d7636ef23bd01f16b2
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-12-19 00:00:08 -0500

    Testing

--- src/routes/users/UsersRouter.js
+++ src/routes/users/UsersRouter.js
@@ -13,7 +13,7 @@ UsersRouter
         
         UsersService.getUser( req.app.get("db"), req.user.id)
             .then( user => {
-                console.log("Hello", user)
+                
                 return res.status(200).json({ user: user[0] });
             })
             .catch(next)
@@ -24,8 +24,6 @@ UsersRouter
     .all(requireAuth)
     .all(express.json())
     .get((req, res, next)=>{
-
-        console.log(req.user.id);
 
         UsersService.getUser( req.app.get("db"), req.user.id)
             .then( user => {
