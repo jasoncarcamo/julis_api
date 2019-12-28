fa445ebfa50255c038fb6598c44c7b87e6c9568e
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-12-28 04:11:14 -0500

    Testing

--- src/routes/login/AuthService.js
+++ src/routes/login/AuthService.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: ".env"});
+require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const {JWT_SECRET} = require("../../../config");
