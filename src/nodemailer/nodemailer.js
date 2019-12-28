e1c06daeb513a26d3d8dceb9d98988755b04da1f
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-12-28 03:17:35 -0500

    Testing

--- src/nodemailer/nodemailer.js
+++ src/nodemailer/nodemailer.js
@@ -1,4 +1,5 @@
-require("dotenv").config()
+require("dotenv").config("../../env");
+console.log(require("dotenv").config("../../env"));
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
@@ -6,7 +7,7 @@ const transporter = nodemailer.createTransport({
     port: 587,
     auth: {
         user: "jasoncarcamo30@yahoo.com",
-        pass: "igld kywe pcub eerf"
+        pass: process.env.NODE_MAILER_PASS
     }
 });
 
