14b2f70dfb94720d4bfbf928f1cb6993c992766b
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-12-28 04:06:10 -0500

    Testing

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: ".env"});
+require("dotenv").config();
 module.exports = {
     PORT: process.env.PORT || 8000,
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jason:carcamo11@localhost/api-test',
